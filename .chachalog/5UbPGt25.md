575472d896b8711914b57d089ed59499fe72a4e7
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2025-03-17 07:42:27 +0100

    feat: pnpm plugin (#2)

---
+++ .chachalog/5UbPGt25.md
@@ -0,0 +1,6 @@
+---
+# Describe desired version bumps as package: major|minor|patch
+npm:chachalog: patch
+---
+
+pnpm is now supported: `import pnpm from "chachalog/pnpm"` (#2)
---
+++ src/pnpm.ts
@@ -0,0 +1,41 @@
+import process from "node:process";
+import { type Project, findWorkspacePackagesNoCheck } from "@pnpm/workspace.find-packages";
+import { readWorkspaceManifest } from "@pnpm/workspace.read-manifest";
+import type { Manager, Package } from "./index.ts";
+
+/** pnpm plugin. Suppports pnpm-workspaces.yaml and package.{json,json5,yaml} files. */
+export default async function pnpm({
+	cwd = process.cwd(),
+}: {
+	/** Current working directory. */
+	cwd?: string;
+}): Promise<Manager> {
+	const workspaceManifest = await readWorkspaceManifest(cwd);
+	const allProjects = await findWorkspacePackagesNoCheck(cwd, {
+		patterns: workspaceManifest?.packages,
+	});
+	const packages = allProjects
+		.filter((ws): ws is Project & { manifest: { name: string; version: string } } =>
+			Boolean(ws.manifest.name && !ws.manifest.private && ws.manifest.version),
+		)
+		.map<[Package, (version: string) => Promise<void>]>((project) => [
+			{
+				prefix: "npm",
+				name: project.manifest.name,
+				version: project.manifest.version,
+				path: project.rootDirRealPath,
+			},
+			(version) => project.writeProjectManifest({ ...project.manifest, version }),
+		]);
+	const map = new WeakMap(packages);
+
+	return {
+		packages: packages.map(([pkg]) => pkg),
+		async setVersion(pkg, version) {
+			const write = map.get(pkg);
+			if (!write) return;
+			await write(version);
+			return true;
+		},
+	};
+}
