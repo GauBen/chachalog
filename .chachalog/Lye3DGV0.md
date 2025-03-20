6df67ca536db457d55a4b2f56e0e5cc72a77f6e9
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2025-03-20 23:46:26 +0100

    feat(comment-pr): more detailed comment (#15)

---
+++ .chachalog/Lye3DGV0.md
@@ -0,0 +1,6 @@
+---
+# Describe desired version bumps as package: major|minor|patch
+chachalog: patch
+---
+
+More detailed PR comment. (#15)
--- src/commands/comment-pr.ts
+++ src/commands/comment-pr.ts
@@ -1,6 +1,9 @@
 import { UsageError } from "clipanion";
+import type { RootContent } from "mdast";
+import { remark } from "remark";
 import type { CommandWithConfig } from "../bin.ts";
 import { processEntries } from "../changelog/process.ts";
+import { writeChangelog } from "../changelog/write.ts";
 
 export default async function commentPr({ config, dir }: CommandWithConfig) {
 	try {
@@ -11,12 +14,37 @@ export default async function commentPr({ config, dir }: CommandWithConfig) {
 		const frontmatter =
 			"---\n# Describe desired version bumps as package: major|minor|patch\n\n---";
 
-		const body = `## 🦜 Chachalog
-
-${bumps.size ? "" : "No changelog entries detected"}
-${[...bumps].map(([key, value]) => `- ${key}: ${value.bump}`).join("\n")}
-
-[Create a new entry](${await config.platform.createChangelogEntryLink(filename, frontmatter)})`;
+		const body = remark().stringify({
+			type: "root",
+			children: [
+				{ type: "heading", depth: 2, children: [{ type: "text", value: "🦜 Chachalog" }] },
+				...((bumps.size > 0
+					? [...bumps].flatMap<RootContent>(([key, value]) => [
+							{
+								type: "html",
+								value: `<details><summary><code>${key}</code> ${value.bump}</summary>`,
+							},
+							{ type: "blockquote", children: writeChangelog(value) },
+							{ type: "html", value: "</details>" },
+						])
+					: [
+							{
+								type: "paragraph",
+								children: [{ type: "text", value: "No changelog entries detected" }],
+							},
+						]) satisfies RootContent[]),
+				{
+					type: "paragraph",
+					children: [
+						{
+							type: "link",
+							url: await config.platform.createChangelogEntryLink(filename, frontmatter),
+							children: [{ type: "text", value: "Create a new entry" }],
+						},
+					],
+				},
+			],
+		});
 
 		await config.platform.upsertChangelogComment(body);
 	} catch (error) {
