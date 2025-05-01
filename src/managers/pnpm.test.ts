d2d1949a69c639b84c66208a0bd8b83e85931903
Gautier Ben Aim <benaim.gautier@gmail.com>
2025-05-01 23:42:11 +0200

    test: test manager bundle

--- src/managers/pnpm.test.ts
+++ src/managers/pnpm.test.ts
@@ -1,39 +1,43 @@
-import test, { after, before } from "node:test";
-import pnpm from "./pnpm.ts";
-import path from "path";
 import assert from "node:assert";
 import { cpSync, rmSync } from "node:fs";
+import test, { afterEach, beforeEach, suite } from "node:test";
+import path from "path";
 
 const cwd = "_fixtures-pnpm-live";
 
-before(() => {
+beforeEach(() => {
 	cpSync("fixtures/pnpm", cwd, { recursive: true, force: true });
 });
 
-after(() => {
+afterEach(() => {
 	rmSync(cwd, { recursive: true, force: true });
 });
 
-test("pnpm", async () => {
-	const manager = await pnpm({ cwd });
-	assert.deepStrictEqual(manager.packages, [
-		{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
-		{ name: "foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo") },
-		{ name: "sub-foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo/sub-foo") },
-	]);
-	await manager.setVersion?.(manager.packages[0], "1.0.2");
-	await manager.setVersion?.(manager.packages[1], "1.0.0");
-	await manager.setVersion?.(manager.packages[2], "0.2.0");
-	// Ensure packages are matched using object equality
-	await manager.setVersion?.(
-		{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
-		"2.0.0",
-	);
+suite("pnpm", async () => {
+	for (const entrypoint of ["./pnpm.ts", "chachalog/pnpm"]) {
+		test(entrypoint, async () => {
+			const { default: pnpm } = (await import(entrypoint)) as typeof import("./pnpm.ts");
+			const manager = await pnpm({ cwd });
+			assert.deepStrictEqual(manager.packages, [
+				{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
+				{ name: "foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo") },
+				{ name: "sub-foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo/sub-foo") },
+			]);
+			await manager.setVersion?.(manager.packages[0], "1.0.2");
+			await manager.setVersion?.(manager.packages[1], "1.0.0");
+			await manager.setVersion?.(manager.packages[2], "0.2.0");
+			// Ensure packages are matched using object equality
+			await manager.setVersion?.(
+				{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
+				"2.0.0",
+			);
 
-	const after = await pnpm({ cwd });
-	assert.deepStrictEqual(after.packages, [
-		{ name: "bar", version: "1.0.2", path: path.resolve(cwd, "packages/bar") },
-		{ name: "foo", version: "1.0.0", path: path.resolve(cwd, "packages/foo") },
-		{ name: "sub-foo", version: "0.2.0", path: path.resolve(cwd, "packages/foo/sub-foo") },
-	]);
+			const after = await pnpm({ cwd });
+			assert.deepStrictEqual(after.packages, [
+				{ name: "bar", version: "1.0.2", path: path.resolve(cwd, "packages/bar") },
+				{ name: "foo", version: "1.0.0", path: path.resolve(cwd, "packages/foo") },
+				{ name: "sub-foo", version: "0.2.0", path: path.resolve(cwd, "packages/foo/sub-foo") },
+			]);
+		});
+	}
 });
--- src/managers/yarn.test.ts
+++ src/managers/yarn.test.ts
@@ -1,39 +1,43 @@
-import test, { after, before } from "node:test";
-import yarn from "./yarn.ts";
-import path from "path";
 import assert from "node:assert";
 import { cpSync, rmSync } from "node:fs";
+import test, { afterEach, beforeEach, suite } from "node:test";
+import path from "path";
 
 const cwd = "_fixtures-yarn-live";
 
-before(() => {
+beforeEach(() => {
 	cpSync("fixtures/yarn", cwd, { recursive: true, force: true });
 });
 
-after(() => {
+afterEach(() => {
 	rmSync(cwd, { recursive: true, force: true });
 });
 
-test("yarn", async () => {
-	const manager = await yarn({ cwd });
-	assert.deepStrictEqual(manager.packages, [
-		{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
-		{ name: "foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo") },
-		{ name: "sub-foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo/sub-foo") },
-	]);
-	await manager.setVersion?.(manager.packages[0], "1.0.2");
-	await manager.setVersion?.(manager.packages[1], "1.0.0");
-	await manager.setVersion?.(manager.packages[2], "0.2.0");
-	// Ensure packages are matched using object equality
-	await manager.setVersion?.(
-		{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
-		"2.0.0",
-	);
+suite("yarn", async () => {
+	for (const entrypoint of ["./yarn.ts", "chachalog/yarn"]) {
+		test(entrypoint, async () => {
+			const { default: yarn } = (await import(entrypoint)) as typeof import("./yarn.ts");
+			const manager = await yarn({ cwd });
+			assert.deepStrictEqual(manager.packages, [
+				{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
+				{ name: "foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo") },
+				{ name: "sub-foo", version: "0.1.2", path: path.resolve(cwd, "packages/foo/sub-foo") },
+			]);
+			await manager.setVersion?.(manager.packages[0], "1.0.2");
+			await manager.setVersion?.(manager.packages[1], "1.0.0");
+			await manager.setVersion?.(manager.packages[2], "0.2.0");
+			// Ensure packages are matched using object equality
+			await manager.setVersion?.(
+				{ name: "bar", version: "1.0.1", path: path.resolve(cwd, "packages/bar") },
+				"2.0.0",
+			);
 
-	const after = await yarn({ cwd });
-	assert.deepStrictEqual(after.packages, [
-		{ name: "bar", version: "1.0.2", path: path.resolve(cwd, "packages/bar") },
-		{ name: "foo", version: "1.0.0", path: path.resolve(cwd, "packages/foo") },
-		{ name: "sub-foo", version: "0.2.0", path: path.resolve(cwd, "packages/foo/sub-foo") },
-	]);
+			const after = await yarn({ cwd });
+			assert.deepStrictEqual(after.packages, [
+				{ name: "bar", version: "1.0.2", path: path.resolve(cwd, "packages/bar") },
+				{ name: "foo", version: "1.0.0", path: path.resolve(cwd, "packages/foo") },
+				{ name: "sub-foo", version: "0.2.0", path: path.resolve(cwd, "packages/foo/sub-foo") },
+			]);
+		});
+	}
 });
