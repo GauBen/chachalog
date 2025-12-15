b4fd7e8c4a61e92e5587456201b4559ceb621da5
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-12-15 08:58:24 +0100

    emulate make-fetch-happen

---
+++ make-fetch-happen.js
@@ -0,0 +1,6 @@
+// Replace `make-fetch-happen` with the built-in `fetch` implementation
+fs.writeFileSync(
+	path.join(execEnv.buildDir, "package.json"),
+	JSON.stringify({ name: "make-fetch-happen", version: "1.0.0" }),
+);
+fs.writeFileSync(path.join(execEnv.buildDir, "index.js"), "module.exports = fetch");
